fe65f84f55b657c8e9b5c28367955f59d394536d
Nate <nelliott.ca@gmail.com>
2017-01-30 09:33:14 -0400

    template update, hey, that rhymes.

--- www/views.py
+++ www/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.views import View
-
 from django.http import HttpResponse
-
-# Create your views here.
 
 
 class IndexView(View):
-
+    template_name = 'index.html'
     def get(self, request):
-        return HttpResponse('gtg')
 
+
+
+
+
+
+
+
+        return render(request,self.template_name)
